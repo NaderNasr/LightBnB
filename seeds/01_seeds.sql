1c7f1287dbe7ba3be968f4d1cebf57e956bd52d1
Nader Nasr <nadernasr7@gmail.com>
2022-02-09 18:10:26 -0500

    mock data

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -8,9 +8,9 @@ VALUES(1,'property_1', 'description', 'https://images.pexels.com/photos/2121121/
       (2,'property_2', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 400, 4, 5, 4, 'Canada', 'St Catherine Street', 'Montreal', 'Qc', 'M4Y1S9'),
       (3,'property_3', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 100, 14, 5, 3, 'Canada', 'Church Street', 'Toronto', 'ON', 'M4Y1X9');
 
-INSERT INTO reservations (start_date, end_date, property_id, guest_id)
-VALUES('2020-10-12', '2021-12-26', 1, 2),
-      ('2011-05-12', '2011-06-01', 3, 4) ;
+INSERT INTO reservations (property_id, guest_id, start_date, end_date)
+VALUES(1, 2, '2020-10-12', '2021-12-26'),
+      (3, 4, '2011-05-12', '2011-06-01') ;
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
 VALUES (2, 5, 8, 3, 'message'),
